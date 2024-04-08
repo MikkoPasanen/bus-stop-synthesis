4716caaba719302c1c4c3c17c57611c14595b2e4
Eph57 <nieppola.konsta@protonmail.com>
2024-04-08 13:59:36 +0300

    App.jsx: import DisplayLocation.jsx, display location on page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,157 +2,152 @@ import { useState } from "react";
 import fetchJourneys from "./fetchJourneys";
 import validBusStops from "./validBusStops";
 import axios from "axios";
+import DisplayLocation from "./DisplayLocation";
 
 function App() {
-    // Bus stop announcement ID, testing purposes for now
-    const [id, setId] = useState("");
+  // Bus stop announcement ID, testing purposes for now
+  const [id, setId] = useState("");
 
-    // Default: Line 6, TAMK bus stop
-    const [linenro, setLinenro] = useState(6);
+  // Default: Line 6, TAMK bus stop
+  const [linenro, setLinenro] = useState(6);
 
-    // Users location
-    const [latitude, setLatitude] = useState(61.503178);
-    const [longitude, setLongitude] = useState(23.812778);
+  // Users location
+  const [latitude, setLatitude] = useState(61.503178);
+  const [longitude, setLongitude] = useState(23.812778);
 
-    // Bus tracking
-    const [tracking, setTracking] = useState("not tracking");
+  // Bus tracking
+  const [tracking, setTracking] = useState("not tracking");
 
-    // Valid bus input
-    const [validBusInput, setValidBusInput] = useState(true);
+  // Valid bus input
+  const [validBusInput, setValidBusInput] = useState(true);
 
+  /**
+   * Fetches the prefix and the announcement mp3 files from the backend
+   * and plays them in sequence
+   */
+  const fetchAndPlayMP3 = async () => {
+    try {
+      // Get the mp3 files from browsers localstorage
+      let prefixData = localStorage.getItem("prefix");
+      let announcementData = localStorage.getItem(id);
 
-    /**
-     * Fetches the prefix and the announcement mp3 files from the backend
-     * and plays them in sequence
-     */
-    const fetchAndPlayMP3 = async () => {
-        try {
+      // If the mp3 files are not in the localstorage, fetch them from the backend
+      // and save them to the localstorage
 
-            // Get the mp3 files from browsers localstorage
-            let prefixData = localStorage.getItem("prefix");
-            let announcementData = localStorage.getItem(id);
+      if (!prefixData) {
+        const prefixResponse = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/prefix`
+        );
+        prefixData = prefixResponse.data.prefix;
+        localStorage.setItem("prefix", prefixData);
+      }
 
-            // If the mp3 files are not in the localstorage, fetch them from the backend
-            // and save them to the localstorage
+      if (!announcementData) {
+        const announcementResponse = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/mp3/${id}`
+        );
+        announcementData = announcementResponse.data.mp3;
+        localStorage.setItem(id, announcementData);
+      }
 
-            if (!prefixData) {
-                const prefixResponse = await axios.get(
-                    `${import.meta.env.VITE_API_URL}/api/prefix`
-                );
-                prefixData = prefixResponse.data.prefix;
-                localStorage.setItem("prefix", prefixData);
-            }
+      // Make the audio files from the base64 strings
+      const prefixAudio = new Audio(`data:audio/mpeg;base64,${prefixData}`);
+      const announcementAudio = new Audio(
+        `data:audio/mpeg;base64,${announcementData}`
+      );
 
-            if (!announcementData) {
-                const announcementResponse = await axios.get(
-                    `${import.meta.env.VITE_API_URL}/api/mp3/${id}`
-                );
-                announcementData = announcementResponse.data.mp3;
-                localStorage.setItem(id, announcementData);
-            }
-
-
-            // Make the audio files from the base64 strings
-            const prefixAudio = new Audio(
-                `data:audio/mpeg;base64,${prefixData}`
-            );
-            const announcementAudio = new Audio(
-                `data:audio/mpeg;base64,${announcementData}`
-            );
-
-            // First play the prefix audio and then the announcement audio
-            // after the prefix audio has ended
-            prefixAudio.play();
-            prefixAudio.onended = () => {
-                announcementAudio.play();
-            };
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    /**
-     * Checks if the bus line number is valid by
-     * comparing it to the valid bus lines array
-     *
-     * @param {*} linenro
-     * @returns true if the bus line number is valid, false otherwise
-     */
-    const checkValidBusStop = (linenro) => {
-        const validBusLine = validBusStops.includes(linenro);
-        setValidBusInput(validBusLine);
-        return validBusLine;
+      // First play the prefix audio and then the announcement audio
+      // after the prefix audio has ended
+      prefixAudio.play();
+      prefixAudio.onended = () => {
+        announcementAudio.play();
+      };
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-                marginTop: "10rem",
-            }}
+  /**
+   * Checks if the bus line number is valid by
+   * comparing it to the valid bus lines array
+   *
+   * @param {*} linenro
+   * @returns true if the bus line number is valid, false otherwise
+   */
+  const checkValidBusStop = (linenro) => {
+    const validBusLine = validBusStops.includes(linenro);
+    setValidBusInput(validBusLine);
+    return validBusLine;
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        marginTop: "10rem",
+      }}
+    >
+      <div>
+        <input
+          type="text"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          placeholder="Enter bus stop ID"
+          style={{ marginBottom: "1rem" }}
+        />
+        <button onClick={() => fetchAndPlayMP3()}>Fetch and Play</button>
+      </div>
+      <div>Currently tracked bus: {tracking}</div>
+      <div>
+        <input
+          type="text"
+          value={linenro}
+          onChange={(e) => setLinenro(e.target.value)}
+          placeholder="Enter the bus line"
+          style={{ marginBottom: "1rem" }}
+        />
+        <input
+          type="text"
+          value={latitude}
+          onChange={(e) => setLatitude(e.target.value)}
+          placeholder="Enter your latitude"
+          style={{ marginBottom: "1rem" }}
+        />
+        <input
+          type="text"
+          value={longitude}
+          onChange={(e) => setLongitude(e.target.value)}
+          placeholder="Enter your longitude"
+          style={{ marginBottom: "1rem" }}
+        />
+        <button
+          onClick={() => {
+            if (checkValidBusStop(linenro)) {
+              if (tracking == "not tracking") {
+                fetchJourneys
+                  .fetchBus(linenro, latitude, longitude)
+                  .then((data) => setTracking(data));
+              } else {
+                setTracking("not tracking");
+              }
+            }
+          }}
         >
-            <div>
-                <input
-                    type="text"
-                    value={id}
-                    onChange={(e) => setId(e.target.value)}
-                    placeholder="Enter bus stop ID"
-                    style={{ marginBottom: "1rem" }}
-                />
-                <button onClick={() => fetchAndPlayMP3()}>
-                    Fetch and Play
-                </button>
-            </div>
-            <div>Currently tracked bus: {tracking}</div>
-            <div>
-                <input
-                    type="text"
-                    value={linenro}
-                    onChange={(e) => setLinenro(e.target.value)}
-                    placeholder="Enter the bus line"
-                    style={{ marginBottom: "1rem" }}
-                />
-                <input
-                    type="text"
-                    value={latitude}
-                    onChange={(e) => setLatitude(e.target.value)}
-                    placeholder="Enter your latitude"
-                    style={{ marginBottom: "1rem" }}
-                />
-                <input
-                    type="text"
-                    value={longitude}
-                    onChange={(e) => setLongitude(e.target.value)}
-                    placeholder="Enter your longitude"
-                    style={{ marginBottom: "1rem" }}
-                />
-                <button
-                    onClick={() => {
-                        if (checkValidBusStop(linenro)) {
-                            if (tracking == "not tracking") {
-                                fetchJourneys
-                                    .fetchBus(linenro, latitude, longitude)
-                                    .then((data) => setTracking(data));
-                            } else {
-                                setTracking("not tracking");
-                            }
-                        }
-                    }}
-                >
-                    Connect to bus
-                </button>
-            </div>
-            {!validBusInput && (
-                <div style={{ color: "red" }}>
-                    The bus line number is not valid!
-                </div>
-
-            )}
+          Connect to bus
+        </button>
+        <div>
+          <h1>User's Location</h1>
+          <DisplayLocation />
         </div>
-    );
+      </div>
+      {!validBusInput && (
+        <div style={{ color: "red" }}>The bus line number is not valid!</div>
+      )}
+    </div>
+  );
 }
 
 export default App;
