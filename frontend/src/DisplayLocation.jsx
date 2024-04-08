5a1fc07743f1faa4efb695695594cfa2d088d02c
Eph57 <nieppola.konsta@protonmail.com>
2024-04-08 14:39:11 +0300

    DisplayLocation.jsx: new useEffect(): Check if user within set point

--- frontend/src/DisplayLocation.jsx
+++ frontend/src/DisplayLocation.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import haversine from "haversine-distance";
 
 const DisplayLocation = () => {
   const [latitude, setLatitude] = useState(null);
@@ -22,6 +23,30 @@ const DisplayLocation = () => {
     }
   }, []);
 
+  // Temprorary hardcoded location of stop Keskustori H
+  const stopLat = 61.49754;
+  const stopLong = 23.761522;
+
+  const tempStop = [stopLat, stopLong];
+  const userLocation = [latitude, longitude];
+
+  useEffect(() => {
+    if (latitude !== null && longitude !== null) {
+      const distance = haversine(
+        { latitude: latitude, longitude: longitude },
+        { latitude: stopLat, longitude: stopLong }
+      );
+      console.log("Distance:", distance); // For testing purposes, you can remove this line later
+      // Check if distance is within X kilometers (adjust X as needed)
+      if (distance <= 200000) {
+        // Implement audio playback
+        console.log("User is within distance from the specific point.");
+      } else {
+        console.log("Not in distance");
+      }
+    }
+  }, [latitude, longitude]);
+
   return (
     <div>
       {error && <p>Error: {error}</p>}
@@ -30,6 +55,7 @@ const DisplayLocation = () => {
           Latitude: {latitude}, Longitude: {longitude}
         </p>
       )}
+      <p>Distance: {haversine(tempStop, userLocation)}</p>
     </div>
   );
 };
