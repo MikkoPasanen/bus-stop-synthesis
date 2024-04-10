ffd6551dc1c5161555138664f8a0a5d9b6ff958a
Mikko Pasanen <pasanemikko@gmail.com>
2024-04-10 21:06:37 +0300

    Add tracked bus data fetcing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import fetchJourneys from "./util/fetchJourneys.js";
 import validBusLines from "./util/validBusLines.js";
 import DisplayLocation from "./components/DisplayLocation.jsx";
@@ -20,6 +20,69 @@ function App() {
 
   // Valid bus line input
   const [validBusInput, setValidBusInput] = useState(true);
+
+  // Everytime the tracking state changes, fetch the bus info
+  useEffect(() => {
+      let timeoutId;
+
+      // Fetch bus info every 5 seconds recursively
+      const fetchAndSchedule = async () => {
+          if (tracking !== "not tracking") {
+              await fetchBusInfo(tracking);
+              timeoutId = setTimeout(fetchAndSchedule, 5000);
+          }
+      };
+
+      fetchAndSchedule();
+
+      // When the component unmounts, clear the timeout so it stops fetching
+      return () => {
+          if (timeoutId) {
+              clearTimeout(timeoutId);
+          }
+      };
+  }, [tracking]);
+
+  /**
+   * Fetches the bus info from the given vehicleId
+   * and plays the mp3 file if the bus is arriving
+   * in less than 30 seconds
+   *
+   * @param {*} vehicleId
+   */
+  const fetchBusInfo = (vehicleId) => {
+
+    // Fetch the bus info from the given vehicleId
+    fetch(`http://data.itsfactory.fi/journeys/api/1/vehicle-activity?vehicleRef=${vehicleId}`)
+      .then((res) => res.json())
+      .then((data) => {
+
+        // Get the next arrival time and the stop ID
+        let nextArrivalTime = data.body[0].monitoredVehicleJourney.onwardCalls[0].expectedArrivalTime;
+        let stopId = data.body[0].monitoredVehicleJourney.onwardCalls[0].stopPointRef;
+
+        // Parse the time and calculate the time difference
+        let parsedTime = new Date(nextArrivalTime);
+        let currentTime = new Date();
+        let timeDifference = parsedTime - currentTime;
+
+        // Calculate the time difference,
+        // dividing difference (milliseconds) by 30000 (milliseconds)
+        // to get the difference in minutes
+        // So if the result is less than 1, the bus is arriving in less than 30 seconds
+        let timeDifferenceInMinutes = Math.floor(timeDifference / 30000);
+
+        // Parse the stop ID to get the last part of the URL
+        let parsedStopId = stopId.split('/').pop();
+
+        console.log(timeDifferenceInMinutes);
+        console.log(parsedStopId);
+
+        if (timeDifferenceInMinutes <= 1) {
+          fetchAndPlayMP3(parsedStopId)
+        }
+    });
+  };
 
   /**
    * Checks if the bus line number is valid by
