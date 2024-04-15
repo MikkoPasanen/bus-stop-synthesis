2c6c0d0e5282298a5eaf0ec33c1869df868baf31
Eph57 <nieppola.konsta@protonmail.com>
2024-04-15 18:18:23 +0300

    getPosition.js: Make function into a promise

--- frontend/src/util/getPosition.js
+++ frontend/src/util/getPosition.js
@@ -1,19 +1,25 @@
 const getUserPosition = (setLatitude, setLongitude, setError) => {
-  const successHandler = (position) => {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
-  };
+  return new Promise((resolve, reject) => {
+    const successHandler = (position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      resolve(); // Resolve the promise when position is successfully retrieved
+    };
 
-  const errorHandler = (err) => {
-    setError(err.message);
-  };
+    const errorHandler = (err) => {
+      setError(err.message);
+      reject(err); // Reject the promise if there's an error
+    };
 
-  // Attempt to get user's position
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
-  } else {
-    setError("Geolocation is not supported by this browser.");
-  }
+    // Attempt to get user's position
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
+    } else {
+      const errorMessage = "Geolocation is not supported by this browser.";
+      setError(errorMessage);
+      reject(new Error(errorMessage)); // Reject the promise if geolocation is not supported
+    }
+  });
 };
 
 export default getUserPosition;
